be8e5d95040121fe4b0b13d4543399af24867eb9
perrychen901105 <chenzhipeng901105@gmail.com>
2014-12-11 00:50:41 +0800

    add simple usage about string

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,9 @@
+// Playground - noun: a place where people can play
+
+import Foundation
+
+var greeting = "hello world".capitalizedString
+var alternateGreeting = greeting
+alternateGreeting += " and beyond!"
+println(alternateGreeting)
+println(greeting)
