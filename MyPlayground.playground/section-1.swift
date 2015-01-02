74af078872156f6909a8a612efc1ddc58432b27b
perrychen901105 <chenzhipeng901105@gmail.com>
2015-01-02 23:56:52 +0800

    66

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -7,3 +7,16 @@ var alternateGreeting = greeting
 alternateGreeting += " and beyond!"
 println(alternateGreeting)
 println(greeting)
+
+var radius = 4
+let pi = 3.14159
+let million = 1_000_000
+var area = Double(radius) * Double(radius) * pi
+
+var overflow = Int.max &+ 1
+
+let alwaysTrue = true
+
+var address = (742, "Evergreen Terrace")
+println(address.0)
+println(address.1)
