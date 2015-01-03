1b9952df4830fa1c473c72822504abcc99d30ceb
perrychen901105 <chenzhipeng901105@gmail.com>
2015-01-04 00:02:55 +0800

    update for codes

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -2,21 +2,51 @@
 
 import Foundation
 
-var greeting = "hello world".capitalizedString
-var alternateGreeting = greeting
-alternateGreeting += " and beyond!"
-println(alternateGreeting)
-println(greeting)
+/*
+    Base
+*/
+//var greeting = "hello world".capitalizedString
+//var alternateGreeting = greeting
+//alternateGreeting += " and beyond!"
+//println(alternateGreeting)
+//println(greeting)
+//
+//var radius = 4
+//let pi = 3.14159
+//let million = 1_000_000
+//var area = Double(radius) * Double(radius) * pi
+//
+//var overflow = Int.max &+ 1
+//
+//let alwaysTrue = true
 
-var radius = 4
-let pi = 3.14159
-let million = 1_000_000
-var area = Double(radius) * Double(radius) * pi
+/*
+Tuple test
+*/
 
-var overflow = Int.max &+ 1
+//var address = (number: 742, street: "Evergreen Terrace")
+//let (house, street) = address
+//println(address.number)
+//println(street)
 
-let alwaysTrue = true
+/*
+    String interpolation
+*/
+//var address = (742, "Evergreen Terrace")
+//let (house, street) = address
+//println("I live at " + String(house) + ", " + street)
+//println("I live at \(house), \(street)")
+//let str = "I live at \(house + 10), \(street.uppercaseString)"
+//println(str)
 
-var address = (742, "Evergreen Terrace")
-println(address.0)
-println(address.1)
+
+/**
+    Control Flow
+*/
+
+let greeting = "Swift by Tutorials Rocks!"
+var range = Range(start: 1, end: 6) //1...5
+for i in range {
+    println("\(i) - \(greeting)")
+    
+}
